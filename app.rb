00fa74039fbb92d195a56cfa28c1da41414397c3
Amritpal Maan <sonny_maan@yahoo.com>
2020-06-23 19:01:41 +0000

    playing with sinatra before I start on the pokemon clone

--- app.rb
+++ app.rb
@@ -2,8 +2,9 @@ require 'sinatra/base'
 
 class PokeBattle < Sinatra::Base
   get '/' do
-    "Hello!"
+    erb(:index)
   end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
