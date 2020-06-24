d8f8c5ae4dba9ed72570f7a8c3eedb3235425911
Amritpal Maan <sonny_maan@yahoo.com>
2020-06-24 17:33:19 +0000

    wrote first feature test

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,9 @@
-feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
+feature 'Entering Player Names' do
+  scenario 'submitting names' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in :player_1_name, with: 'Sonny'
+    fill_in :player_2_name, with: 'Person'
+    click_button 'Submit'
+    expect(page).to have_content 'Sonny vs Person'
   end
 end
