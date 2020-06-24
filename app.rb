0425b8b602e3d8e49b2f28c34d1efad8aaa17e5e
Amritpal Maan <sonny_maan@yahoo.com>
2020-06-24 17:24:09 +0000

    Setting up Capybara for testing

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 
 class PokeBattle < Sinatra::Base
   get '/' do
-    'Hello PokeBattle!'
+    'Testing infrastructure working!'
   end
 
   # start the server if ruby file executed directly
