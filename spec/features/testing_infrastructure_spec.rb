ac887c64a58d9ab48a6dab94a5fd4454bc6a553b
Amritpal Maan <sonny_maan@yahoo.com>
2020-06-24 17:43:54 +0000

    feature test passing

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -4,6 +4,6 @@ feature 'Entering Player Names' do
     fill_in :player_1_name, with: 'Sonny'
     fill_in :player_2_name, with: 'Person'
     click_button 'Submit'
-    expect(page).to have_content 'Sonny vs Person'
+    expect(page).to have_content 'Sonny vs. Person'
   end
 end
