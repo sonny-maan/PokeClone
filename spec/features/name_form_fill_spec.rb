acf31b847bd1f715b3a6bcd5c55f67a4423ed0b1
Amritpal Maan <sonny_maan@yahoo.com>
2020-06-29 14:36:35 +0000

    refactored first test

--- spec/features/name_form_fill_spec.rb
+++ spec/features/name_form_fill_spec.rb
@@ -3,7 +3,7 @@ describe 'Entering Player Names', type: :feature do
     visit '/'
     fill_in 'player1', with: 'Sonny'
     fill_in 'player2', with: 'Person'
-    find(:xpath, "//input[@id='player-name-submit']").click
+    click_button 'player-name-submit'
     expect(page).to have_content 'Sonny'
     expect(page).to have_content 'Person'
   }
