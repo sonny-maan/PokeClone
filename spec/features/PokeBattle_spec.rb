4dd2eff191ced58ae55729a1926c629a5625278c
Amritpal Maan <sonny_maan@yahoo.com>
2020-06-29 14:44:27 +0000

    added new feature test and made test pass

--- spec/features/PokeBattle_spec.rb
+++ spec/features/PokeBattle_spec.rb
@@ -9,3 +9,13 @@ describe 'Entering Player Names', type: :feature do
   }
 end
 
+feature 'View hit points' do
+  scenario 'see Player 2 hit points' do
+    visit '/'
+    fill_in 'player1', with: 'Sonny'
+    fill_in 'player2', with: 'Person'
+    click_button 'player-name-submit'
+    expect(page).to have_content 'Person: 60HP'
+  end
+end
+
