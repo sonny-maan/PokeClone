08bade3c34a4f97e38571d6fb471b45438a0e469
Amritpal Maan <sonny_maan@yahoo.com>
2020-06-29 14:50:54 +0000

    added player 1 hitpoints test

--- spec/features/PokeBattle_spec.rb
+++ spec/features/PokeBattle_spec.rb
@@ -7,6 +7,10 @@ describe 'Entering Player Names', type: :feature do
 end
 
 feature 'View hit points' do
+  scenario 'see Player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Sonny: 60HP'
+  end
   scenario 'see Player 2 hit points' do
     sign_in_and_play
     expect(page).to have_content 'Person: 60HP'
